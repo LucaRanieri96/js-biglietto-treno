881f854ac7ae65a5cebd37664cd5738f136fdd4c
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-08 14:46:01 +0100

    Ask customer's km and Age

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*
-TRACCIA
+1.TRACCIA :
 
 Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
@@ -7,6 +7,23 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 • va applicato uno sconto del 20% per i minorenni
 • va applicato uno sconto del 40% per gli over 65.
 • L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-
 */
-alert("hello!")
+/* 
+2.STRUMENTI : 
+
+• PROMPT
+• CONSOLE.LOG
+• MATH
+• IF/ELSE
+• INNERHTML
+*/
+
+// 3. ESECUZIONE :
+
+/* chiedo all'utente quanti km vuole percorrere e l'età,
+   inizializzo due variabili e le mostro in console */
+
+const clientKm = prompt("Inserisci il numero dei km che vuoi percorrere");
+const clientAge = prompt("Inserisci la tua età");
+console.log(clientKm , clientAge);
+
